7abb0f1b2a8085a6726e8c5daff16af025e7c1ae
IMackerI <moojkoo@gmail.com>
2022-05-24 09:17:07 +0200

    Fixed GameManager Problems

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,36 +5,24 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance { get; private set; }
-    public enum State { MENU, PLAY, ATTACK, WIN, LOSE };
+    public enum State { ATTACK_1, ATTACK_2, WIN_1, WIN_2 };
 
-    public GameObject PlayerPrefab;
-    public GameObject EnemyPrefab;
-    public GameObject menuScreen;
-    public GameObject playerHealth;
-    public GameObject enemyHealth;
-    public GameObject playerDamage;
-    public GameObject enemyDamage;
-    public GameObject playerCoins;
-    public GameObject enemyCoins;
-    
+    public Entity Player1;
+    public Entity Player2;
+
     State _state;
-    GameObject _player;
-    GameObject _enemy;
-
-    bool _enemyTurn = false;
-    bool _attacking = false;
 
     public void Start()
     {
         instance = this;
-        Switchstate(State.PLAY);
+        Switchstate(State.ATTACK_1);
     }
+    
 
     public void Switchstate(State newState, float delay = 0)
     {
         StartCoroutine(SwitchDelay(newState, delay));
     }
-
     IEnumerator SwitchDelay(State newState, float delay)
     {
         //Debug.Log("Switching state");
@@ -45,123 +33,55 @@ public class GameManager : MonoBehaviour
         //Debug.Log("Switched state");
     }
 
-    IEnumerator DamageDelay(SliderScript damage, float delay, bool playerA)
-    {
-        yield return new WaitForSeconds(delay);
-        if (playerA)
-        {
-            damage.SetValue(_enemy.GetComponent<Enemy>().health);
-        }
-        else
-        {
-            damage.SetValue(_player.GetComponent<Clovek>().health);
-        }
-        
-    }
-
     void BeginState(State newState)
     {
         switch (newState)
         {
-            case State.MENU:
-                menuScreen.SetActive(true);
+            case State.ATTACK_1:
+                Player1.Attack();
+                break;
+            case State.ATTACK_2:
+                Player2.Attack();
+                break;
+            case State.WIN_1:
+                break;
+            case State.WIN_2:
+                break;
+            default:
                 break;
         }
-
     }
 
     void EndState()
     {
         switch (_state)
         {
-            case State.MENU:
-                menuScreen.SetActive(false);
-                CreatePlayer(100, 40, 1000);
-                CreateEnemy(100, 10, 1000);
-                playerHealth.GetComponent<SliderScript>().SetMaxValue(100);
-                enemyHealth.GetComponent<SliderScript>().SetMaxValue(100);
+            case State.ATTACK_1:
+                if (Player2.Alive())
+                {
+                    Switchstate(State.ATTACK_2);
+                }
+                else
+                {
+                    Switchstate(State.WIN_1);
+                }
                 break;
-            case State.ATTACK:
-                _attacking = false;
+            case State.ATTACK_2:
+                if (Player1.Alive())
+                {
+                    Switchstate(State.ATTACK_1);
+                }
+                else
+                {
+                    Switchstate(State.WIN_2);
+                }
+                break;
+            case State.WIN_1:
+                break;
+            case State.WIN_2:
+                break;
+            default:
                 break;
         }
     }
-    
-    // Update is called once per frame
-    void Update()
-    {
-        switch (_state)
-        {
-            case State.MENU:
-                break;
-            case State.PLAY:
-                if (_player.GetComponent<Clovek>().health <= 0)
-                {
-                    Debug.Log("You lose");
-                    Switchstate(State.LOSE);
-                }
-                else if(_enemy.GetComponent<Enemy>().health <= 0)
-                {
-                    Debug.Log("You win");
-                    Switchstate(State.WIN);
-                }
-                else if (!_attacking)
-                {
-                    Debug.Log(_state);
-                    if (_enemyTurn)
-                    {
-                        _enemyTurn = false;
-                        _attacking = true;
-                        PlayerAttack();
-                        int damage = (int)(Random.Range(0.8f, 1.2f) * _player.GetComponent<Clovek>().damage);
-                        _enemy.GetComponent<Enemy>().health -= damage;
-                        StartCoroutine(DamageDelay(enemyHealth.GetComponent<SliderScript>(), 1.5f, true));
-                    }
-                    else
-                    {
-                        _enemyTurn = true;
-                        _attacking = true;
-                        EnemyAttack();
-                        int damage = (int)(Random.Range(0.8f, 1.2f) * _enemy.GetComponent<Enemy>().damage);
-                        _player.GetComponent<Clovek>().health -= damage;
-                        StartCoroutine(DamageDelay(playerHealth.GetComponent<SliderScript>(), 1.5f, false));
-                    }
-                    Switchstate(State.ATTACK, 4);
-                }
-                break;
-            case State.ATTACK:
-                Switchstate(State.PLAY);
-                break;
-            case State.WIN:
-                break;
-            case State.LOSE:
-                break;
-
-        }
-    }
-
-    void CreatePlayer(int health, int damage, int coins)
-    {
-        _player = Instantiate(PlayerPrefab);
-        _player.GetComponent<Clovek>().health = health;
-        _player.GetComponent<Clovek>().damage = damage;
-        _player.GetComponent<Clovek>().coins = coins;
-    }
-    void CreateEnemy(int health, int damage, int coins)
-    {
-        _enemy = Instantiate(EnemyPrefab);
-        _enemy.GetComponent<Enemy>().health = health;
-        _enemy.GetComponent<Enemy>().damage = damage;
-        _enemy.GetComponent<Enemy>().coins = coins;
-    }
-
-    void PlayerAttack()
-    {
-        _player.GetComponent<Clovek>().Attack();
-    }
-    void EnemyAttack()
-    {
-        _enemy.GetComponent<Enemy>().Attack();
-    }
-
 }
